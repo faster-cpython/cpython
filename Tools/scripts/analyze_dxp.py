4ed250ed8fe2f7592874aade6a3ba8d321c9960c
Eric Snow <ericsnowcurrently@gmail.com>
2021-03-23 10:13:42 -0600

    Allow the module if not a DXPAIRS build (plus cleanup).

--- Tools/scripts/analyze_dxp.py
+++ Tools/scripts/analyze_dxp.py
@@ -6,9 +6,6 @@ in the current process, and, if Python was also built with -DDXPAIRS,
 will tell you which instruction _pairs_ were executed most frequently,
 which may help in choosing new instructions.
 
-If Python was built without -DDYNAMIC_EXECUTION_PROFILE, importing
-this module will raise a RuntimeError.
-
 If you're running a script you want to profile, a simple way to get
 the common pairs is:
 
@@ -20,19 +17,58 @@ $ PYTHONPATH=$PYTHONPATH:<python_srcdir>/Tools/scripts \
 > open('/tmp/some_file', 'w').write(s)
 """
 
-import copy
+import json
 import opcode
 import operator
+import os
 import sys
-import threading
-
-if not hasattr(sys, "getdxp"):
-    raise RuntimeError("Can't import analyze_dxp: Python built without"
-                       " -DDYNAMIC_EXECUTION_PROFILE.")
 
 
-_profile_lock = threading.RLock()
-_cumulative_profile = sys.getdxp()
+if hasattr(sys, "getdxp"):
+    import copy
+    import threading
+
+
+    _profile_lock = threading.RLock()
+    _cumulative_profile = sys.getdxp()
+
+
+    def reset_profile():
+        """Forgets any execution profile that has been gathered so far."""
+        with _profile_lock:
+            sys.getdxp()  # Resets the internal profile
+            global _cumulative_profile
+            _cumulative_profile = sys.getdxp()  # 0s out our copy.
+
+
+    def merge_profile():
+        """Reads sys.getdxp() and merges it into this module's cached copy.
+
+        We need this because sys.getdxp() 0s itself every time it's called."""
+
+        with _profile_lock:
+            new_profile = sys.getdxp()
+            if has_pairs(new_profile):
+                for first_inst in range(len(_cumulative_profile)):
+                    for second_inst in range(len(_cumulative_profile[first_inst])):
+                        _cumulative_profile[first_inst][second_inst] += (
+                            new_profile[first_inst][second_inst])
+            else:
+                for inst in range(len(_cumulative_profile)):
+                    _cumulative_profile[inst] += new_profile[inst]
+
+
+    def snapshot_profile():
+        """Returns the cumulative execution profile until this call."""
+        with _profile_lock:
+            merge_profile()
+            return copy.deepcopy(_cumulative_profile)
+
+
+def load_profile(filename=None):
+    with open(filename or 'dxp.json') as infile:
+        return json.load(infile)
+
 
 # If Python was built with -DDXPAIRS, sys.getdxp() returns a list of
 # lists of ints.  Otherwise it returns just a list of ints.
@@ -43,38 +79,6 @@ def has_pairs(profile):
     return len(profile) > 0 and isinstance(profile[0], list)
 
 
-def reset_profile():
-    """Forgets any execution profile that has been gathered so far."""
-    with _profile_lock:
-        sys.getdxp()  # Resets the internal profile
-        global _cumulative_profile
-        _cumulative_profile = sys.getdxp()  # 0s out our copy.
-
-
-def merge_profile():
-    """Reads sys.getdxp() and merges it into this module's cached copy.
-
-    We need this because sys.getdxp() 0s itself every time it's called."""
-
-    with _profile_lock:
-        new_profile = sys.getdxp()
-        if has_pairs(new_profile):
-            for first_inst in range(len(_cumulative_profile)):
-                for second_inst in range(len(_cumulative_profile[first_inst])):
-                    _cumulative_profile[first_inst][second_inst] += (
-                        new_profile[first_inst][second_inst])
-        else:
-            for inst in range(len(_cumulative_profile)):
-                _cumulative_profile[inst] += new_profile[inst]
-
-
-def snapshot_profile():
-    """Returns the cumulative execution profile until this call."""
-    with _profile_lock:
-        merge_profile()
-        return copy.deepcopy(_cumulative_profile)
-
-
 def common_instructions(profile):
     """Returns the most common opcodes in order of descending frequency.
 
@@ -82,15 +86,19 @@ def common_instructions(profile):
       (opcode, opname, # of occurrences)
 
     """
+    # Ignore opcodes with a count of 0.
+    result = [v for v in _common_instructions(profile) if v[-1] > 0]
+    result.sort(key=operator.itemgetter(2), reverse=True)
+    return result
+
+
+def _common_instructions(profile):
     if has_pairs(profile) and profile:
         inst_list = profile[-1]
     else:
         inst_list = profile
-    result = [(op, opcode.opname[op], count)
-              for op, count in enumerate(inst_list)
-              if count > 0]
-    result.sort(key=operator.itemgetter(2), reverse=True)
-    return result
+    for op, count in enumerate(inst_list):
+        yield (op, opcode.opname[op], count)
 
 
 def common_pairs(profile):
@@ -104,13 +112,16 @@ def common_pairs(profile):
     """
     if not has_pairs(profile):
         return []
-    result = [((op1, op2), (opcode.opname[op1], opcode.opname[op2]), count)
-              # Drop the row of single-op profiles with [:-1]
-              for op1, op1profile in enumerate(profile[:-1])
-              for op2, count in enumerate(op1profile)
-              if count > 0]
+    result = [v for v in _common_pairs(profile) if v[-1] > 0]
     result.sort(key=operator.itemgetter(2), reverse=True)
     return result
+
+
+def _common_pairs(profile):
+    # Drop the row of single-op profiles with [:-1]
+    for op1, op1profile in enumerate(profile[:-1]):
+        for op2, count in enumerate(op1profile):
+            yield ((op1, op2), (opcode.opname[op1], opcode.opname[op2]), count)
 
 
 def render_common_pairs(profile=None):
@@ -122,8 +133,17 @@ def render_common_pairs(profile=None):
 
     """
     if profile is None:
+        if not hasattr(sys, "getdxp"):
+            raise RuntimeError("missing dxp profile: Python built without -DDYNAMIC_EXECUTION_PROFILE")
         profile = snapshot_profile()
-    def seq():
-        for _, ops, count in common_pairs(profile):
-            yield "%s: %s\n" % (count, ops)
-    return ''.join(seq())
+    if not has_pairs(profile):
+        return ''
+    lines = _render_common_pairs(profile) + ['']
+    return os.linesep.join(lines)
+
+
+def _render_common_pairs(profile):
+    pairs = [v for v in _common_pairs(profile) if v[-1] > 0]
+    pairs.sort(key=operator.itemgetter(2), reverse=True)
+    for _, ops, count in pairs:
+        yield f'{count:>6} - {ops}'
