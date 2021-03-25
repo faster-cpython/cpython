1f27594acf139b606134a2d8973c7da18b31be87
Eric Snow <ericsnowcurrently@gmail.com>
2021-03-25 12:43:31 -0600

    Clean up a little.

--- Python/_perf.c
+++ Python/_perf.c
@@ -6,28 +6,6 @@
 #include "Python.h"
 #include "pycore_initconfig.h"  // _PyArgv
 
-static FILE * _trace_file = NULL;
-
-void
-_PyPerf_Trace(const char *id)
-{
-    struct timespec time = {0,0};
-    clock_gettime(CLOCK_MONOTONIC, &time);
-    if (_trace_file) {
-        fprintf(_trace_file, "%ld.%ld %s\n", time.tv_sec, time.tv_nsec, id);
-    }
-}
-
-void
-_PyPerf_TraceOp(int op)
-{
-    struct timespec time = {0,0};
-    clock_gettime(CLOCK_MONOTONIC, &time);
-    if (_trace_file) {
-        fprintf(_trace_file, "%ld.%ld <op %d>\n", time.tv_sec, time.tv_nsec, op);
-    }
-}
-
 // timespec_sub is provided by <timespec.h> in C11.
 static struct timespec
 timespec_sub(struct timespec after, struct timespec before)
@@ -46,17 +24,69 @@ timespec_sub(struct timespec after, struct timespec before)
     return elapsed;
 }
 
-static long
-_get_trace_cost_ns(void)
+static inline struct timespec
+_now(void)
 {
+    struct timespec time = {0,0};
+    clock_gettime(CLOCK_MONOTONIC, &time);
+    return time;
+}
+
+static const char *
+_get_filename_default(const char *name)
+{
+    const char *suffix = ".trace";
+    char *filename = (char *)PyMem_Malloc(strlen(name) + strlen(suffix) + 1);
+    sprintf(filename, "%s%s", name, suffix);
+    return filename;
+}
+
+static const char *
+_get_filename(const char *name)
+{
+    // XXX Look up an optional env var?
+    return _get_filename_default(name);
+}
+
+static inline void
+_log_msg(FILE *logfile, const char *msg)
+{
+    struct timespec time = _now();
+    fprintf(logfile, "%ld.%ld %s\n", time.tv_sec, time.tv_nsec, msg);
+}
+
+static inline void
+_log_op(FILE *logfile, int op)
+{
+    struct timespec time = _now();
+    fprintf(logfile, "%ld.%ld <op %d>\n", time.tv_sec, time.tv_nsec, op);
+}
+
+static FILE *_fake_api_file = NULL;
+
+// Duplicate _PyPerf_Trace() without relying on _trace_file.
+void
+_fake_api(const char *id)
+{
+    if (_fake_api_file) {
+        _log_msg(_fake_api_file, id);
+    }
+}
+
+static long
+_get_trace_cost_ns(FILE *f)
+{
+    assert(f);
+    _fake_api_file = f;
+    long pos_orig = ftell(f);
+
     // XXX Repeat multiple times for a stable result?
-    struct timespec before = {0,0};
-    struct timespec after = {0,0};
-    clock_gettime(CLOCK_MONOTONIC, &before);
-    assert(_trace_file);
-    _PyPerf_Trace("???");
-    clock_gettime(CLOCK_MONOTONIC, &after);
-    fseek(_trace_file, 0, SEEK_SET);
+    struct timespec before = _now();
+    _fake_api("???");
+    struct timespec after = _now();
+
+    fseek(f, pos_orig, SEEK_SET);
+    _fake_api_file = NULL;
 
     struct timespec elapsed = timespec_sub(after, before);
     assert(elapsed.tv_sec == 0);
@@ -72,14 +102,36 @@ _print_argv(FILE *f, int argc, char **argv)
     }
 }
 
+static FILE * _trace_file = NULL;
+
+//======================
+// the public API
+//======================
+
+void
+_PyPerf_Trace(const char *id)
+{
+    if (_trace_file) {
+        _log_msg(_trace_file, id);
+    }
+}
+
+void
+_PyPerf_TraceOp(int op)
+{
+    if (_trace_file) {
+        _log_op(_trace_file, op);
+    }
+}
+
 void
 _PyPerf_TraceInit(_PyArgv *args)
 {
-    const char *filename = "eval_loop.trace";
+    const char *filename = _get_filename("eval_loop");
     _trace_file = fopen(filename, "w");
     assert(_trace_file != NULL);
 
-    long cost = _get_trace_cost_ns();
+    long cost = _get_trace_cost_ns(_trace_file);
 
     // Write a "header".
     assert(args);
