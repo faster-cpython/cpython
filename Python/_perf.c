020038f7ffc9610b35981859d789913fc4f98549
Eric Snow <ericsnowcurrently@gmail.com>
2021-03-25 12:00:09 -0600

    Record how long a trace takes.

--- Python/_perf.c
+++ Python/_perf.c
@@ -28,6 +28,41 @@ _PyPerf_TraceOp(int op)
     }
 }
 
+// timespec_sub is provided by <timespec.h> in C11.
+static struct timespec
+timespec_sub(struct timespec after, struct timespec before)
+{
+    struct timespec elapsed = {
+        .tv_sec = after.tv_sec - before.tv_sec,
+        .tv_nsec = after.tv_nsec - before.tv_nsec,
+    };
+    if (elapsed.tv_nsec < 0) {
+        elapsed.tv_sec -= 1;
+#ifndef NSEC_PER_SEC
+#define NSEC_PER_SEC 1000000000L
+#endif
+        elapsed.tv_nsec += NSEC_PER_SEC;
+    }
+    return elapsed;
+}
+
+static long
+_get_trace_cost_ns(void)
+{
+    // XXX Repeat multiple times for a stable result?
+    struct timespec before = {0,0};
+    struct timespec after = {0,0};
+    clock_gettime(CLOCK_MONOTONIC, &before);
+    assert(_trace_file);
+    _PyPerf_Trace("???");
+    clock_gettime(CLOCK_MONOTONIC, &after);
+    fseek(_trace_file, 0, SEEK_SET);
+
+    struct timespec elapsed = timespec_sub(after, before);
+    assert(elapsed.tv_sec == 0);
+    return elapsed.tv_nsec;
+}
+
 static void
 _print_argv(FILE *f, int argc, char **argv)
 {
@@ -44,6 +79,8 @@ _PyPerf_TraceInit(_PyArgv *args)
     _trace_file = fopen(filename, "w");
     assert(_trace_file != NULL);
 
+    long cost = _get_trace_cost_ns();
+
     // Write a "header".
     assert(args);
     if (args->use_bytes_argv) {
@@ -55,6 +92,7 @@ _PyPerf_TraceInit(_PyArgv *args)
         // XXX Use Py_EncodeLocaleRaw()?
         fprintf(_trace_file, "# argv: <unknown>");
     }
+    fprintf(_trace_file, "# per-trace: %ld ns\n", cost);
     fprintf(_trace_file, "\n");  // Add a blank line.
 
     _PyPerf_Trace("<init>");
